3301adbd0cfb88dcbec90cf6ce44ba88135f44d8
Bartosz Sudol <bartosz.sudol1@outlook.com>
2025-07-29 12:25:59 +0200

    GTM integration

--- lib/gtag.tsx
+++ lib/gtag.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script'
 import { useEffect, Suspense } from 'react' // Import Suspense
 
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
+export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
@@ -51,31 +52,43 @@ const AnalyticsNavigationEvents = () => {
 }
 
 export const GoogleAnalytics = () => {
-  if (!GA_TRACKING_ID) {
+  if (!GA_TRACKING_ID && !GTM_ID) {
+    console.warn('Google Analytics and Google Tag Manager IDs are not set')
     return null
   }
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-      {/* Wrap the component using hooks in Suspense */}
+      {/* Google Tag Manager */}
+      {GTM_ID && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      )}
+
+      {/* GA_TRACKING_ID && !GTM_ID && */} (
+      {GA_TRACKING_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `}
+          </Script>
+        </>
+      )}
       <Suspense fallback={null}>
         <AnalyticsNavigationEvents />
       </Suspense>
